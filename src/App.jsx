bc1a6b70c5b87c2d4d82639e468dab8e87866021
LAURIDS0 <laur762d@gmail.com>
2026-02-26 00:11:01 +0100

    Klokken er 00:10

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,52 @@
-import logo from './logo.svg';
+import TextType from "./components/TextType";
+import AnimatedList from './components/AnimatedList'
+import LiquidEther from './components/LiquidEther';
+
+const items = [
+  'Item 1',
+  'Item 2',
+  'Item 3',
+  'Item 4', 'Item 5', 'Item 6', 'Item 7', 'Item 8', 'Item 9', 'Item 10'
+  ]; 
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ width: '100%', height: 600, position: 'relative' }} className="App">
+      <LiquidEther
+        colors={[ '#5227FF', '#FF9FFC', '#B19EEF' ]}
+        mouseForce={20}
+        cursorSize={100}
+        isViscous
+        viscous={30}
+        iterationsViscous={32}
+        iterationsPoisson={32}
+        resolution={0.5}
+        isBounce={false}
+        autoDemo
+        autoSpeed={0.5}
+        autoIntensity={2.2}
+        takeoverDuration={0.25}
+        autoResumeDelay={3000}
+        autoRampDuration={0.6}
+        color0="#5227FF"
+        color1="#FF9FFC"
+        color2="#B19EEF"
+    />
+      <TextType
+        text="Laurids B. Christoffersen"
+        typingSpeed={50}
+        showCursor
+        cursorCharacter="_"
+        cursorBlinkDuration={0.5}
+      />
+      <AnimatedList
+        items={items}
+        onItemSelect={(item, index) => console.log(item, index)}
+        showGradients = {false}
+        enableArrowNavigation
+        displayScrollbar
+      />
     </div>
   );
 }
